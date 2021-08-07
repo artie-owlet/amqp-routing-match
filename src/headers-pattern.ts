9013eeed1fe327bcceecd01d7a7ae459c565a9e5
artie-owlet <artie-owlet@protonmail.com>
2021-08-08 02:38:43 +0300

    done

---
+++ src/headers-pattern.ts
@@ -0,0 +1,47 @@
+interface IMessageHeaders {
+    [key: string]: any;
+}
+
+interface IRoutingHeaders extends IMessageHeaders {
+    'x-match': 'all' | 'any';
+}
+
+export class HeadersPattern {
+    private all: boolean;
+    private headersPattern = {} as IMessageHeaders;
+
+    constructor(routingHeaders: IRoutingHeaders) {
+        this.all = routingHeaders['x-match'] === 'all';
+        for (const key in routingHeaders) {
+            if (!key.startsWith('x-')) {
+                this.headersPattern[key] = routingHeaders[key] as unknown;
+            }
+        }
+    }
+
+    public match(appHeaders: IMessageHeaders): boolean {
+        if (this.all) {
+            return this.matchAll(appHeaders);
+        } else {
+            return this.matchAny(appHeaders);
+        }
+    }
+
+    private matchAll(appHeaders: IMessageHeaders): boolean {
+        for (const key in this.headersPattern) {
+            if (appHeaders[key] !== this.headersPattern[key]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private matchAny(appHeaders: IMessageHeaders): boolean {
+        for (const key in this.headersPattern) {
+            if (appHeaders[key] === this.headersPattern[key]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { TopicPattern } from './topic-pattern';
+export { HeadersPattern } from './headers-pattern';
